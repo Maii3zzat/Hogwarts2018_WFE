a8bf7ebd5659c4d70b44efd3221233e534b1cb78
Mai Ezzat <maii3zzat@hotmail.com>
2021-05-01 07:04:31 +0200

    @app init

---
+++ sortinghat.js
@@ -0,0 +1,440 @@
+function check(){
+
+  var question1=document.quiz.question1.value;
+  var question2=document.quiz.question2.value;
+  var question3=document.quiz.question3.value;
+  var question4=document.quiz.question4.value;
+  var question5=document.quiz.question5.value;
+  var question6=document.quiz.question6.value;
+  var question7=document.quiz.question7.value;
+  var question8=document.quiz.question8.value;
+  var question9=document.quiz.question9.value;
+  var question10=document.quiz.question10.value;
+  var question11=document.quiz.question11.value;
+  var question12=document.quiz.question12.value;
+  var question13=document.quiz.question13.value;
+  var question14=document.quiz.question14.value;
+  var question15=document.quiz.question15.value;
+  var question16=document.quiz.question16.value;
+  var question17=document.quiz.question17.value;
+  var question18=document.quiz.question18.value;
+  var question19=document.quiz.question19.value;
+  var question20=document.quiz.question20.value;
+  var question21=document.quiz.question21.value;
+  var question22=document.quiz.question22.value;
+  var question23=document.quiz.question23.value;
+  var question24=document.quiz.question24.value;
+  var question25=document.quiz.question25.value;
+  var question26=document.quiz.question26.value;
+  var question27=document.quiz.question27.value;
+  var question28=document.quiz.question28.value;
+  var question29=document.quiz.question29.value;
+  var gryffindor=0;
+  var slytherin=0;
+  var ravenclaw=0;
+  var hufflepuff=0;
+  var house=0;
+
+  if(question1=="a"){
+    gryffindor++;
+  }
+  else if (question1=="b"){
+    slytherin++;
+  }
+  else if (question1=="c"){
+    ravenclaw++;
+  }
+  else if(question1=="d"){
+    hufflepuff++;
+  }
+
+  if(question2=="a2"){
+    gryffindor++;
+  }
+  else if (question2=="b2"){
+    slytherin++;
+  }
+  else if (question2=="c2"){
+    ravenclaw++;
+  }
+  else if(question2=="d2"){
+    hufflepuff++;
+  }
+
+  if(question3=="a3"){
+    gryffindor++;
+  }
+  else if (question3=="b3"){
+    slytherin++;
+  }
+  else if (question3=="c3"){
+    ravenclaw++;
+  }
+  else if(question3=="d3"){
+    hufflepuff++;
+  }
+
+  if(question4=="a4"){
+    gryffindor++;
+  }
+  else if (question4=="b4"){
+    slytherin++;
+  }
+  else if (question4=="c4"){
+    ravenclaw++;
+  }
+  else if(question4=="d4"){
+    hufflepuff++;
+  }
+
+  if(question5=="a5"){
+    gryffindor++;
+  }
+  else if (question5=="b5"){
+    slytherin++;
+  }
+  else if (question5=="c5"){
+    ravenclaw++;
+  }
+  else if(question5=="d5"){
+    hufflepuff++;
+  }
+
+  if(question6=="a6"){
+    gryffindor++;
+  }
+  else if (question6=="b6"){
+    slytherin++;
+  }
+  else if (question6=="c6"){
+    ravenclaw++;
+  }
+  else if(question6=="d6"){
+    hufflepuff++;
+  }
+
+  if(question7=="a7"){
+    gryffindor++;
+  }
+  else if (question7=="b7"){
+    slytherin++;
+  }
+  else if (question7=="c7"){
+    ravenclaw++;
+  }
+  else if(question7=="d7"){
+    hufflepuff++;
+  }
+
+  if(question8=="a8"){
+    gryffindor++;
+  }
+  else if (question8=="b8"){
+    slytherin++;
+  }
+  else if (question8=="c8"){
+    ravenclaw++;
+  }
+  else if(question8=="d8"){
+    hufflepuff++;
+  }
+
+  if(question9=="a9"){
+    gryffindor++;
+  }
+  else if (question9=="b9"){
+    slytherin++;
+  }
+  else if (question9=="c9"){
+    ravenclaw++;
+  }
+  else if(question9=="d9"){
+    hufflepuff++;
+  }
+
+  if(question10=="a10"){
+    gryffindor++;
+  }
+  else if (question10=="b10"){
+    slytherin++;
+  }
+  else if (question10=="c10"){
+    ravenclaw++;
+  }
+  else if(question10=="d10"){
+    hufflepuff++;
+  }
+
+  if(question11=="a11"){
+    gryffindor++;
+  }
+  else if (question11=="b11"){
+    slytherin++;
+  }
+  else if (question11=="c11"){
+    ravenclaw++;
+  }
+  else if(question11=="d11"){
+    hufflepuff++;
+  }
+
+  if(question12=="a12"){
+    gryffindor++;
+  }
+  else if (question12=="b12"){
+    slytherin++;
+  }
+  else if (question12=="c12"){
+    ravenclaw++;
+  }
+  else if(question12=="d12"){
+    hufflepuff++;
+  }
+
+  if(question13=="a13"){
+    gryffindor++;
+  }
+  else if (question13=="b13"){
+    slytherin++;
+  }
+  else if (question13=="c13"){
+    ravenclaw++;
+  }
+  else if(question13=="d13"){
+    hufflepuff++;
+  }
+
+  if(question14=="a14"){
+    gryffindor++;
+  }
+  else if (question14=="b14"){
+    slytherin++;
+  }
+  else if (question14=="c14"){
+    ravenclaw++;
+  }
+  else if(question14=="d14"){
+    hufflepuff++;
+  }
+
+  if(question15=="a15"){
+    gryffindor++;
+  }
+  else if (question15=="b15"){
+    slytherin++;
+  }
+  else if (question15=="c15"){
+    ravenclaw++;
+  }
+  else if(question15=="d15"){
+    hufflepuff++;
+  }
+
+  if(question16=="a16"){
+    gryffindor++;
+  }
+  else if (question16=="b16"){
+    slytherin++;
+  }
+  else if (question16=="c16"){
+    ravenclaw++;
+  }
+  else if(question16=="d16"){
+    hufflepuff++;
+  }
+
+  if(question17=="a17"){
+    gryffindor++;
+  }
+  else if (question17=="b17"){
+    slytherin++;
+  }
+  else if (question17=="c17"){
+    ravenclaw++;
+  }
+  else if(question17=="d17"){
+    hufflepuff++;
+  }
+
+  if(question18=="a18"){
+    gryffindor++;
+  }
+  else if (question18=="b18"){
+    slytherin++;
+  }
+  else if (question18=="c18"){
+    ravenclaw++;
+  }
+  else if(question18=="d18"){
+    hufflepuff++;
+  }
+
+  if(question19=="a19"){
+    gryffindor++;
+  }
+  else if (question19=="b19"){
+    slytherin++;
+  }
+  else if (question19=="c19"){
+    ravenclaw++;
+  }
+  else if(question19=="d19"){
+    hufflepuff++;
+  }
+
+  if(question20=="a20"){
+    gryffindor++;
+  }
+  else if (question20=="b20"){
+    slytherin++;
+  }
+  else if (question20=="c20"){
+    ravenclaw++;
+  }
+  else if(question20=="d20"){
+    hufflepuff++;
+  }
+
+  if(question21=="a21"){
+    gryffindor++;
+  }
+  else if (question21=="b21"){
+    slytherin++;
+  }
+  else if (question21=="c21"){
+    ravenclaw++;
+  }
+  else if(question21=="d21"){
+    hufflepuff++;
+  }
+
+  if(question22=="a22"){
+    gryffindor++;
+  }
+  else if (question22=="b22"){
+    slytherin++;
+  }
+  else if (question22=="c22"){
+    ravenclaw++;
+  }
+  else if(question22=="d22"){
+    hufflepuff++;
+  }
+
+  if(question23=="a23"){
+    gryffindor++;
+  }
+  else if (question23=="b23"){
+    slytherin++;
+  }
+  else if (question23=="c23"){
+    ravenclaw++;
+  }
+  else if(question23=="d23"){
+    hufflepuff++;
+  }
+
+  if(question24=="a24"){
+    gryffindor++;
+  }
+  else if (question24=="b24"){
+    slytherin++;
+  }
+  else if (question24=="c24"){
+    ravenclaw++;
+  }
+  else if(question24=="d24"){
+    hufflepuff++;
+  }
+
+  if(question25=="a25"){
+    gryffindor++;
+  }
+  else if (question25=="b25"){
+    slytherin++;
+  }
+  else if (question25=="c25"){
+    ravenclaw++;
+  }
+  else if(question25=="d25"){
+    hufflepuff++;
+  }
+
+  if(question26=="a26"){
+    gryffindor++;
+  }
+  else if (question26=="b26"){
+    slytherin++;
+  }
+  else if (question26=="c26"){
+    ravenclaw++;
+  }
+  else if(question26=="d26"){
+    hufflepuff++;
+  }
+
+  if(question27=="a27"){
+    gryffindor++;
+  }
+  else if (question27=="b27"){
+    slytherin++;
+  }
+  else if (question27=="c27"){
+    ravenclaw++;
+  }
+  else if(question27=="d27"){
+    hufflepuff++;
+  }
+
+  if(question28=="a28"){
+    gryffindor++;
+  }
+  else if (question28=="b28"){
+    slytherin++;
+  }
+  else if (question28=="c28"){
+    ravenclaw++;
+  }
+  else if(question28=="d28"){
+    hufflepuff++;
+  }
+
+  if(question29=="a29"){
+    gryffindor+=3;
+  }
+  else if (question29=="b29"){
+    slytherin+=3;
+  }
+  else if (question29=="c29"){
+    ravenclaw+=3;
+  }
+  else if(question29=="d29"){
+    hufflepuff+=3;
+  }
+
+
+
+if (gryffindor > slytherin && gryffindor > ravenclaw && gryffindor > hufflepuff){
+  house=gryffindor;
+  document.getElementById("hogwartshouse").innerHTML="Gryffindor!";
+  document.getElementById("after_submit").style.visibility = "visible";
+}
+else if (slytherin > gryffindor && slytherin > ravenclaw && slytherin > hufflepuff){
+  house=slytherin;
+  document.getElementById("hogwartshouse").innerHTML="Slytherin!";
+  document.getElementById("after_submit").style.visibility = "visible";
+}
+else if (ravenclaw > gryffindor && ravenclaw > slytherin && ravenclaw > hufflepuff){
+  house=ravenclaw;
+  document.getElementById("hogwartshouse").innerHTML="Ravenclaw!";
+  document.getElementById("after_submit").style.visibility = "visible";
+}
+else if (hufflepuff > gryffindor && hufflepuff > slytherin && hufflepuff > ravenclaw){
+  house=hufflepuff;
+  document.getElementById("hogwartshouse").innerHTML="Hufflepuff!";
+  document.getElementById("after_submit").style.visibility = "visible";
+}
+
+
+
+}
